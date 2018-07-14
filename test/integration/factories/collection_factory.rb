f9814eb9d403dbcba94cf69d4b44892d4bbe2842
Artem <temikus@google.com>
2018-07-14 17:40:51 +1000

    Try to enable async destroy to speed up tests

--- test/integration/factories/collection_factory.rb
+++ test/integration/factories/collection_factory.rb
@@ -7,7 +7,7 @@ class CollectionFactory
     @resource_counter = 0
   end
 
-  def cleanup(async = false)
+  def cleanup(async = true)
     resources = @subject.all.select { |resource| resource.name.start_with? PREFIX }
     resources.each { |r| r.destroy(async) }
     resources.each { |r| Fog.wait_for { !@subject.all.map(&:identity).include? r.identity } }
